5baf9e85e813f82a460360eb6efd94fa7c4e7270
Yoonsoo Nam <yoonsoon@usc.edu>
2023-01-27 17:03:53 -0800

    Changed color scheme

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,7 +12,7 @@ const Hero = ({initRef}) => {
                 <h1 className="text-4xl text-center font-bold text-white text-gradient">KOREAN BUSINESS STUDENT ORGANIZATION</h1>
                 <p className="text-white text-center text-l font-poppins font-normal mt-4">USC's Official Korean Business Student Cohort</p>
                 <button 
-                    className="bg-white text-black mt-8 px-8 py-2 rounded-lg hover:bg-secondary"
+                    className="bg-white text-black mt-8 px-8 py-2 rounded-lg hover:bg-secondary hover:text-white transition duration-300 ease-in-out"
                     onClick={onButtonClick}
                 >
                     Learn More
